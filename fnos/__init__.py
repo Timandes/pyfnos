8447df7d948018faae306b39504c0ba7c7e8eada
Timandes White <timands@gmail.com>
2025-11-03 23:24:43 +0800

    chore: bump versions

--- fnos/__init__.py
+++ fnos/__init__.py
@@ -24,6 +24,6 @@ from .sac import SAC
 from .system_info import SystemInfo
 from .user import User
 
-__version__ = "0.1.0"
+__version__ = "0.5.0"
 
 __all__ = ["FnosClient", "Store", "ResourceMonitor", "SAC", "SystemInfo", "User"]
